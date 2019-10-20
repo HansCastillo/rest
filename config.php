ea4b9eb6d1fe85afb027dca275186a65f0cc7341
david <sasori1156@hotmail.com>
2019-10-20 18:01:01 -0500

    primero

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$db = [
+    'host' => 'localhost8',
+    'username' => 'root',
+    'password' => '47655232',
+    'db' => 'blog' //Cambiar al nombre de tu base de datos
+];
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    header('http/1.1 202');    
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        
+    }
+?>
